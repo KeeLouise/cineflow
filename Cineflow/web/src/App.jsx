dd671bd6dfa3b60836833a810a3b5f22de3f5adc
Kiera Reidy <keelouise@yahoo.com>
2025-08-20 10:16:42 +0100

    Protected route setup for dashboard. KR 20/08/2025

--- Cineflow/web/src/App.jsx
+++ Cineflow/web/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard.jsx";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -16,8 +17,12 @@ export default function App() {
         {/* Registraton Page */}
         <Route path="/register" element={<Register />} /> 
 
-        {/* Dashboard Page */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Dashboard Page - Protected */}
+        <Route path="/dashboard" element={<PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      } 
+      />
       </Routes>
   );
 }
