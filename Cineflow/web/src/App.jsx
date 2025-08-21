6e7bdd09fdd52cb01c86cde719ef7cc19a8acd85
Kiera Reidy <keelouise@yahoo.com>
2025-08-21 20:47:25 +0100

    Fixing so viewport is 100%. KR 21/08/2025

--- Cineflow/web/src/App.jsx
+++ Cineflow/web/src/App.jsx
@@ -4,25 +4,29 @@ import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import Dashboard from "@/pages/Dashboard";
+import Footer from "./components/Footer.jsx";
 import PrivateRoute from "@/components/PrivateRoute";
 
 export default function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </>
+      <main className="flex-grow-1 w-100">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
