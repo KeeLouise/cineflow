00f692b89281851469a64866bc37070fe83811ee
Kiera Reidy <keelouise@yahoo.com>
2025-09-30 22:12:17 +0100

    Further updated. KR 30/09/2025

--- Cineflow/api/views/profile.py
+++ Cineflow/api/views/profile.py
@@ -6,27 +6,28 @@ from rest_framework.response import Response
 from ..serializers import UserProfileSerializer
 from ..models import UserProfile
 
+
 @api_view(["GET", "PATCH"])
 @permission_classes([IsAuthenticated])
 @parser_classes([MultiPartParser, FormParser, JSONParser])
 def me_profile(request):
     user = request.user
+    profile, _ = UserProfile.objects.get_or_create(user=user)
 
     if request.method == "GET":
-        return Response(UserProfileSerializer(user).data, status=200)
+        return Response(UserProfileSerializer(profile).data, status=200)
 
     data = request.data.copy()
     remove_flag = str(data.get("remove_avatar", "")).lower() in ("1", "true", "yes", "on")
 
-    ser = UserProfileSerializer(user, data=data, partial=True)
+    ser = UserProfileSerializer(profile, data=data, partial=True)
     ser.is_valid(raise_exception=True)
     instance = ser.save()
 
     if remove_flag:
-        prof, _ = UserProfile.objects.get_or_create(user=instance)
-        if prof.avatar:
-            prof.avatar.delete(save=False)
-        prof.avatar = None
-        prof.save(update_fields=["avatar", "updated_at"])
+        if instance.avatar:
+            instance.avatar.delete(save=False)
+        instance.avatar = None
+        instance.save(update_fields=["avatar", "updated_at"])
 
     return Response(UserProfileSerializer(instance).data, status=200)
