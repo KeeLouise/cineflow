345bc6acf1581bf6b925e28a9556ff20684da331
Kiera Reidy <keelouise@yahoo.com>
2025-09-30 21:55:03 +0100

    Further test fixes. KR 30/09/2025

--- Cineflow/api/views/profile.py
+++ Cineflow/api/views/profile.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -7,6 +8,7 @@ from ..models import UserProfile
 
 @api_view(["GET", "PATCH"])
 @permission_classes([IsAuthenticated])
+@parser_classes([MultiPartParser, FormParser, JSONParser])
 def me_profile(request):
     user = request.user
 
@@ -14,14 +16,12 @@ def me_profile(request):
         return Response(UserProfileSerializer(user).data, status=200)
 
     data = request.data.copy()
-
     remove_flag = str(data.get("remove_avatar", "")).lower() in ("1", "true", "yes", "on")
 
     ser = UserProfileSerializer(user, data=data, partial=True)
     ser.is_valid(raise_exception=True)
     instance = ser.save()
 
-    # If remove flag is set, clear avatar regardless of whether a file was sent
     if remove_flag:
         prof, _ = UserProfile.objects.get_or_create(user=instance)
         if prof.avatar:
@@ -29,5 +29,4 @@ def me_profile(request):
         prof.avatar = None
         prof.save(update_fields=["avatar", "updated_at"])
 
-    # Return fresh data
     return Response(UserProfileSerializer(instance).data, status=200)
