208eb75439aeaa28a7b98f9eec91ab576626fa83
Kiera Reidy <keelouise@yahoo.com>
2025-10-02 02:11:07 +0100

    Removed duplicate tokens. KR 02/10/2025

--- Cineflow/core/urls.py
+++ Cineflow/core/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path("api/ping/", views.ping, name="api_ping"),
     path("api/secure/", views.secure_view, name="api_secure"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
