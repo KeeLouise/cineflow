c36a96ca06f190e29a57db7d719dcefe2bcbe7f4
Kiera Reidy <keelouise@yahoo.com>
2025-10-03 11:46:40 +0100

    Further tests

--- Cineflow/cineflow/settings.py
+++ Cineflow/cineflow/settings.py
@@ -155,7 +155,6 @@ STORAGES = {
 }
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-WHITENOISE_MANIFEST_STRICT = False
 
 # --- Cloudinary media storage (optional) ---
 CLOUDINARY_URL = os.getenv("CLOUDINARY_URL", "").strip()
